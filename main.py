17932cbe5fb93adbe18bc487681945e98c9528f6
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-07-01 22:19:43 -0300

    feat: embedding endpoint using env variables MAX_SIZE query params
    - /ai max length 300
    - /embedding max length 800
    - .env.example with new variables

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from fastapi import FastAPI
+import os
+from typing import Annotated
+
+from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
+from langchain_openai import OpenAIEmbeddings
+
 from rag import rag_chain
 
+from dotenv import load_dotenv
+load_dotenv()
+
+ENDPOINT_QUERY_AI_MAX_SIZE = int(os.getenv("ENDPOINT_QUERY_AI_MAX_SIZE"))
+ENDPOINT_EMBEDDING_TEXT_MAX_SIZE = int(os.getenv("ENDPOINT_EMBEDDING_TEXT_MAX_SIZE"))
+ENDPOINT_EMBEDDING_MODEL = os.getenv("ENDPOINT_EMBEDDING_MODEL")
 app = FastAPI(
     title="Bora IA API",
     version="0.1.0"
 )
 
 @app.get("/ai")
-async def ai(query: str):
+async def ai(query: Annotated[str, Query(max_length=ENDPOINT_QUERY_AI_MAX_SIZE)]):
+    print(len(query))
     response = rag_chain.invoke({"input": query})
     return response["answer"]
+
+@app.post("/embedding")
+async def embedding(text: Annotated[str, Query(max_length=ENDPOINT_EMBEDDING_TEXT_MAX_SIZE)]):
+    embedder = OpenAIEmbeddings(model=ENDPOINT_EMBEDDING_MODEL)
+    return embedder.embed_query(text)
 
 @app.get("/ping")
 async def root():
