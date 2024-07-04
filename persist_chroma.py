72dce1ea599dacf8123a83be64f5daeb3a4938e3
happyholand <josefelipehol@gamil.com>
2024-07-03 23:25:26 -0300

    feat: add pdf function added to persist chroma (couldn't use api key for some reason to test)

--- persist_chroma.py
+++ persist_chroma.py
@@ -22,8 +22,25 @@ def load_docs():
 
     return docs
 
-docs = load_docs()
+def initialize_chroma() -> None:
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-Chroma.from_documents(documents=splits, persist_directory="./chroma_db", embedding=OpenAIEmbeddings(model="text-embedding-3-large"))
+    docs = load_docs()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    Chroma.from_documents(documents=splits, persist_directory="./chroma_db", embedding=OpenAIEmbeddings(model="text-embedding-3-large"))
+
+def add_ppcs_pdf(*ppc_name : str) -> None:
+    pdf_files = [pdf_file for pdf_file in ppc_name if pdf_file.lower().endswith('.pdf')]
+
+    docs = []
+    for pdf_file in pdf_files:
+        file_path = os.path.join('./data/ppcs', pdf_file)
+        loader = PyMuPDFLoader(file_path)
+        docs.extend(loader.load())
+
+    db = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"))
+    db.add_documents(documents=docs)
+
+add_ppcs_pdf("PPC_Engenharia_Mecatrnica_Bacharelado_-_Natal_-_Atualizado_em_2018-1.pdf")
+
