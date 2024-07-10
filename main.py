1c4ea9075cd5d824e279c1dfebc568fb6ce7e391
Felipe Holanda <91347304+HappyHoland@users.noreply.github.com>
2024-07-09 21:50:11 -0300

    feat: added feedback classifier (#13)
    * feat: added feedback classifier
    
    * feat: updated .env.example and got commented classify function for now
    
    * fix(feedback_classifier): remove unecessary comments
    
    * fix(feedback_classifier): 'agressivo' instead of 'aggressivo'
    
    * fix(main): remove unecessary prints
    
    * chore(gitignore): .idea
    
    * feat(feedback_classifier): env var ENDPOINT_FEEDBACK_MODEL'
    
    ---------
    
    Co-authored-by: happyholand <josefelipehol@gmail.com>

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.responses import RedirectResponse
 from langchain_openai import OpenAIEmbeddings
 
 from rag import rag_chain
+from feedback_classifier import tagging_chain
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -13,14 +14,19 @@ load_dotenv()
 ENDPOINT_QUERY_AI_MAX_SIZE = int(os.getenv("ENDPOINT_QUERY_AI_MAX_SIZE"))
 ENDPOINT_EMBEDDING_TEXT_MAX_SIZE = int(os.getenv("ENDPOINT_EMBEDDING_TEXT_MAX_SIZE"))
 ENDPOINT_EMBEDDING_MODEL = os.getenv("ENDPOINT_EMBEDDING_MODEL")
+ENDPOINT_QUERY_FEEDBACK_CLASSIFY_MAX_SIZE = os.getenv("ENDPOINT_QUERY_FEEDBACK_CLASSIFY_MAX_SIZE")
 app = FastAPI(
     title="Bora IA API",
     version="0.1.0"
 )
 
+@app.get("/feedback_classify")
+async def feedback_classify(query: Annotated[str, Query(max_length=ENDPOINT_QUERY_FEEDBACK_CLASSIFY_MAX_SIZE)]):
+    result = tagging_chain.invoke({"input": query})
+    return result
+
 @app.get("/ai")
 async def ai(query: Annotated[str, Query(max_length=ENDPOINT_QUERY_AI_MAX_SIZE)]):
-    print(len(query))
     response = rag_chain.invoke({"input": query})
     return response["answer"]
 
