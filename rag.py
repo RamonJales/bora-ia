27c357aea18dbb8f16d11c075d4c1126bc1272ae
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2024-08-06 21:35:18 -0300

    feat(rag): document_prompt contendo origem + pagina do texto retirado

--- rag.py
+++ rag.py
@@ -1,10 +1,9 @@
 import os
 from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, BasePromptTemplate, PromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_groq import ChatGroq
-from persist_chroma import load_chroma
 from services.chroma_service import ChromaService
 
 load_dotenv()
@@ -17,6 +16,7 @@ system_prompt = (
     "Você é um assistente para tarefas de perguntas e respostas."
     "Use os seguintes trechos de contexto recuperados para responder à pergunta."
     "Se você não souber a resposta, diga que não sabe."
+    "Você deve deixar explícito os nomes e páginas dos PDFs do contexto que foram usados."
     "\n\n"
     "{context}"
 )
@@ -28,7 +28,12 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-question_answer_chain = create_stuff_documents_chain(llm, prompt)
+document_prompt = PromptTemplate(
+    input_variables=["page_content", "title"],
+    template="[O texto a seguir foi retirado de \"{title}\" na página {page}] {page_content}"
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt, document_prompt=document_prompt)
 
 
 def get_rag_chain():
